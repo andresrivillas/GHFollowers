9aa9ce0dd19c83589d081f8eaa267846cbb54a29
Andres Rivillas <arivillas@enlace.org>
2024-09-03 17:07:43 -0500

    Day two

--- GHFollowers/GHFollowers/Screens/SearchVC.swift
+++ GHFollowers/GHFollowers/Screens/SearchVC.swift
@@ -36,7 +36,7 @@ class SearchVC: UIViewController {
     
     @objc func pushFollowersListVC() {
         guard isUsernameEntered else {
-            print("no username") 
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 😊.", buttonTitle: "Ok")
             return
         }
         
