d67fe3dd651e12e9f65baf8b7a55000fc377406c
Andres Rivillas <arivillas@enlace.org>
2024-09-05 15:16:12 -0500

    Day three

--- GHFollowers/GHFollowers/Custom views/GFAvatarImageView.swift
+++ GHFollowers/GHFollowers/Custom views/GFAvatarImageView.swift
@@ -1,5 +1,5 @@
 //
-//  GFAvatarImageView.swift
+//  GFImageView.swift
 //  GHFollowers
 //
 //  Created by Andres Rivillas on 4/09/24.
@@ -8,13 +8,22 @@
 import UIKit
 
 class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
--- GHFollowers/GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/GHFollowers/Screens/FollowerListVC.swift
@@ -17,7 +17,6 @@ class FollowerListVC: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            
             switch result {
             case .success(let followers):
                 print(followers)
